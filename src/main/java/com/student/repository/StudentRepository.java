8b9aa409065775b7e69b7c7058a2fee603c8a3fe
Голубцов Алексей <golubczov-12@mail.ru>
2021-07-18 20:51:11 +0300

    StudentRepository, добавил JdbcTemplate ошибка(Метод, помеченный @Autowired, вызывается напряму Вместо этого используйте внедрение зависимостей.)

--- src/main/java/com/student/repository/StudentRepository.java
+++ src/main/java/com/student/repository/StudentRepository.java
@@ -1,5 +1,25 @@
 package com.student.repository;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
 public class StudentRepository {
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgres.driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5433/student");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("0992964446");
+        return dataSource;
+    }
+@Autowired
+public JdbcTemplate jdbcTemplate(){
+    return new JdbcTemplate(dataSource());
+}
 }
