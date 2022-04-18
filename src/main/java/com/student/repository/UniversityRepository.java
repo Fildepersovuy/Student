20bafc36ec6a4268a0bebe9874b601f48892c835
Fildepersovuy <golubczov-12@mail.ru>
2022-04-18 19:27:29 +0300

    добавил '%s' и все заработало в UniversityRepository.java

--- src/main/java/com/student/repository/UniversityRepository.java
+++ src/main/java/com/student/repository/UniversityRepository.java
@@ -16,7 +16,7 @@ public class UniversityRepository {
     private JdbcTemplate jdbcTemplate;
 
     public List<UniversityEntity> findByAbbreviation(String abbrev) {
-        String query = String.format("SELECT * FROM universities WHERE universities.abbreviation = %s;\n",abbrev);
+        String query = String.format("SELECT * FROM universities WHERE universities.abbreviation = '%s';\n",abbrev);
         return jdbcTemplate.query(query,new BeanPropertyRowMapper<>(UniversityEntity.class));
     }
     public void addUniversityEntity(List<UniversityDto> universityDtoList){
@@ -25,7 +25,7 @@ public class UniversityRepository {
         UniversityMapper universityMapper = new UniversityMapper();
         for(UniversityDto x: universityDtoList){
             UniversityEntity universityEntity = universityMapper.mapToEntity(x);
-            String query = String.format("INSERT INTO universities (abbreviation, full_title, founding_date, creation_date ) VALUES(%s, %s, %s, %s);\n",universityEntity.getAbbreviation(),universityEntity.getFull_title(),universityEntity.getFounding_date(),universityEntity.getCreation_date());
+            String query = String.format("INSERT INTO universities (abbreviation, full_title, founding_date, creation_date ) VALUES('%s', '%s', '%s', '%s');\n",universityEntity.getAbbreviation(),universityEntity.getFull_title(),universityEntity.getFounding_date(),universityEntity.getCreation_date());
             allQuery+= query;
         }
         jdbcTemplate.execute(allQuery);
