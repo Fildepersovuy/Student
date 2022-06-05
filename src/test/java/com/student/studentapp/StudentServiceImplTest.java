eef5eb12d7f74a2c4bc11178204f966b1e99df73
ICaptainJack <golubczov-12@mail.ru>
2022-06-05 18:08:33 +0300

    создал сам обьект  StudentDto без mock, добавил 2 verify для FileWriterService

--- src/test/java/com/student/studentapp/StudentServiceImplTest.java
+++ src/test/java/com/student/studentapp/StudentServiceImplTest.java
@@ -2,6 +2,7 @@ package com.student.studentapp;
 
 import com.student.dto.StudentDto;
 import com.student.repository.StudentRepository;
+import com.student.service.FileWriterService;
 import com.student.service.impl.StudentServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,24 +12,29 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verify;
 
-@ExtendWith(MockitoExtension.class)
+
 public class StudentServiceImplTest {
 
     @Mock
     private StudentRepository studentRepository;
+
+    @Mock
+    private FileWriterService fileWriterService;
 
     @InjectMocks
     private StudentServiceImpl studentService;
 
     @Test
     void findByNameShouldCallRepositoryTest() throws IOException {
-        StudentDto studentDto =  Mockito.mock(StudentDto.class);
+        List<StudentDto> studentDtoList = new ArrayList<>();
+        StudentDto studentDto = new StudentDto();
         studentRepository.findByName("Sanya");
 
         List<StudentDto> actual = studentService.findByName("Sanya",false,true);
@@ -36,5 +42,7 @@ public class StudentServiceImplTest {
         assertNotNull(actual);
         assertEquals(studentDto,actual);
         verify(studentRepository).findByName("Sanya");
+        verify(fileWriterService).write(studentDtoList);
+        verify(fileWriterService).readableRecord(studentDtoList);
     }
 }
