1aad4ae4611debac969daf2b3082601ae76ee278
ICaptainJack <golubczov-12@mail.ru>
2022-06-06 19:41:06 +0300

    создал сам обьект  StudentDto без mock, добавил 2 verify для FileWriterService

--- src/main/java/com/student/service/impl/StudentServiceImpl.java
+++ src/main/java/com/student/service/impl/StudentServiceImpl.java
@@ -15,10 +15,15 @@ import java.util.stream.Collectors;
 
 @Service
 public class StudentServiceImpl implements StudentService {
-    @Autowired
-    private StudentRepository studentRepository;
-    @Autowired
-    private FileWriterService writeService;
+    private final StudentRepository studentRepository;
+
+    private final FileWriterService writeService;
+
+    public StudentServiceImpl(StudentRepository studentRepository, FileWriterService writeService) {
+        this.studentRepository = studentRepository;
+        this.writeService = writeService;
+    }
+
 
     public List<StudentDto> findByName(String name, boolean saveInFile, boolean saveInFileReadable) throws IOException {
         List<StudentDto> allName = studentRepository.findByName(name)
--- src/test/java/com/student/studentapp/StudentServiceImplTest.java
+++ src/test/java/com/student/studentapp/StudentServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.student.studentapp;
 
+import com.student.domain.StudentEntity;
 import com.student.dto.StudentDto;
 import com.student.repository.StudentRepository;
 import com.student.repository.impl.StudentRepositoryImpl;
@@ -8,24 +9,31 @@ import com.student.service.impl.FileWriterServiceImpl;
 import com.student.service.impl.StudentServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class StudentServiceImplTest {
 
     @Mock
-    private StudentRepositoryImpl studentRepository;
+    private StudentRepository studentRepository;
 
     @Mock
     private FileWriterServiceImpl fileWriterService;
@@ -34,17 +42,26 @@ public class StudentServiceImplTest {
     private StudentServiceImpl studentService;
 
     @Test
-    void findByNameShouldCallRepositoryTest() throws IOException {
-        List<StudentDto> studentDtoList = new ArrayList<>();
-        StudentDto studentDto = new StudentDto();
-        studentRepository.findByName("Sanya");
+    void findByNameShouldCallRepositoryTest() throws IOException, ParseException {
+        Mockito.when(studentRepository.findByName(anyString())).thenReturn(getStudentEntity());
 
         List<StudentDto> actual = studentService.findByName("Sanya",false,true);
 
         assertNotNull(actual);
-        assertEquals(studentDto,actual);
+        assertEquals(1,actual.size());
         verify(studentRepository).findByName("Sanya");
-        verify(fileWriterService).write(studentDtoList);
-        verify(fileWriterService).readableRecord(studentDtoList);
+        verify(fileWriterService).write(actual);
+        verify(fileWriterService).readableRecord(actual);
+    }
+    private List<StudentEntity> getStudentEntity() throws ParseException {
+        StudentEntity studentEntity = new StudentEntity();                                    // дано:
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse("1995-05-05");
+        studentEntity.setFirstName("Sanya");
+        studentEntity.setLastName("Esip");
+        studentEntity.setEmail("se@gmail.ru");
+        studentEntity.setGender("male");
+        studentEntity.setDateOfBirth(date);
+        studentEntity.setCountryOfBirth("Russia");
+        return List.of(studentEntity);
     }
 }
