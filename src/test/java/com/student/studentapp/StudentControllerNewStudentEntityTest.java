0036e726627774ce07c5021ffde4e0d56bc0f3c7
ICaptainJack <golubczov-12@mail.ru>
2022-06-13 14:42:53 +0300

    попытка на юниттест контроллера

--- src/test/java/com/student/studentapp/StudentControllerNewStudentEntityTest.java
+++ src/test/java/com/student/studentapp/StudentControllerNewStudentEntityTest.java
@@ -1,14 +1,19 @@
 package com.student.studentapp;
 
 import com.student.controllers.StudentController;
+import com.student.domain.StudentEntity;
 import com.student.dto.StudentDto;
+import com.student.mapper.StudentMapper;
+import com.student.repository.impl.StudentRepositoryImpl;
 import com.student.service.impl.StudentServiceImpl;
 
+import org.hibernate.mapping.Array;
 import org.hibernate.mapping.Set;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.Validator;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -16,45 +21,57 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.ValidatorFactory;
 
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
-@RunWith(MockitoJUnitRunner.class)
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(StudentController.class)
 public class StudentControllerNewStudentEntityTest {
 
-    private static ValidatorFactory validatorFactory;
-    private static Validator validator;
-
-    @BeforeClass
-    public static void createValidator() {
-        validatorFactory = Validation.buildDefaultValidatorFactory();
-        validator = (Validator) validatorFactory.getValidator();
-    }
-
-    @AfterClass
-    public static void close() {
-        validatorFactory.close();
-    }
-
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private StudentServiceImpl service;
     @Test
-    void newStudentEntityShouldValid(){
-    StudentController studentController = new StudentController();
-    List<StudentDto> studentDtoList = new ArrayList<>();
-    studentController.newStudentEntity(studentDtoList);
+    void newStudentEntityValid() throws Exception {
+        StudentDto studentDto = new StudentDto();
+        List<StudentDto> list = Arrays.asList(studentDto);
 
-//  Set<ConstraintViolation<StudentController>> violations = validator.validate(studentController);
+        given(service.findByName(String.valueOf(list),false,true)).willReturn(list);
 
-    assertFalse(validator.validate(studentController).isEmpty());
+        mvc.perform(get("/student")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
     }
 }
