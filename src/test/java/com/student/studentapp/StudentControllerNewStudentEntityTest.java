3f71ff8c074b1d214cb4c4c3a8abda84a644d76a
ICaptainJack <golubczov-12@mail.ru>
2022-06-28 20:52:37 +0300

    фикс теста StudentControllerNewStudentEntityTest

--- src/test/java/com/student/studentapp/StudentControllerNewStudentEntityTest.java
+++ src/test/java/com/student/studentapp/StudentControllerNewStudentEntityTest.java
@@ -1,83 +1,73 @@
 package com.student.studentapp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.student.controllers.StudentController;
-import com.student.domain.StudentEntity;
 import com.student.dto.StudentDto;
-import com.student.mapper.StudentMapper;
-import com.student.repository.impl.StudentRepositoryImpl;
 import com.student.service.impl.StudentServiceImpl;
 
-import org.hibernate.mapping.Array;
-import org.hibernate.mapping.Set;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.Validator;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.ValidatorFactory;
-
-
-import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
-
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = StudentController.class)
 @WebMvcTest(StudentController.class)
-public class StudentControllerNewStudentEntityTest {
+class StudentControllerNewStudentEntityTest {
 
     @Autowired
     private MockMvc mvc;
     @MockBean
     private StudentServiceImpl service;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     void newStudentEntityValid() throws Exception {
         StudentDto studentDto = new StudentDto();
         List<StudentDto> list = Arrays.asList(studentDto);
 
-        given(service.findByName(String.valueOf(list),false,true)).willReturn(list);
+        given(service.findByName(String.valueOf(list),false,true))
+                .willReturn(list);
 
-        mvc.perform(
-                post("/student")
-                        .contentType(MediaType.APPLICATION_JSON)
-        )
-                .andExpect(status().isCreated())
-                .andExpect((ResultMatcher) content().json(String.valueOf(studentDto)));
+        String contentAsString = mvc.perform(
+                        post("/student")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(getStudentDto()))
+                )
+                .andExpect(status().is2xxSuccessful())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        Assertions.assertEquals("", contentAsString);
+    }
+
+    @SneakyThrows
+    private List<StudentDto> getStudentDto() {
+        StudentDto studentDto = new StudentDto();
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse("1995-05-05");
+        studentDto.setFirstName("Sanya");
+        studentDto.setLastName("Esip");
+        studentDto.setEmail("se@gmail.ru");
+        studentDto.setGender("male");
+        studentDto.setDateOfBirth(date);
+        studentDto.setCountryOfBirth("Russia");
+
+        List<StudentDto> studentDtoList = new ArrayList<>();
+        studentDtoList.add(studentDto); //Р°РЅР°Р»РѕРіРёС‡РЅРѕ РјРѕР¶РЅРѕ СЃРґРµР»Р°С‚СЊ СЃ РїРѕРјРѕС‰СЊСЋ List.of(studentDto)
+        return studentDtoList;
     }
 }
