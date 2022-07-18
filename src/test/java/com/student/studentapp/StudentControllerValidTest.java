66ce107cabd0f76b6f91d64f8ed6a886d365d78b
ICaptainJack <golubczov-12@mail.ru>
2022-07-18 21:38:45 +0300

    написал 3 теста для валидации(имя, фамилия, эмейл)(рабочии)

--- src/test/java/com/student/studentapp/StudentControllerValidTest.java
+++ src/test/java/com/student/studentapp/StudentControllerValidTest.java
@@ -32,7 +32,7 @@ public class StudentControllerValidTest {
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    void newStudentEntityValid() throws Exception {
+    void newStudentEntityNoValidFirstName() throws Exception {
         List<StudentDto> list = new ArrayList<>();
         StudentDto studentDto = new StudentDto();
         Date date = new SimpleDateFormat("yyyy-MM-dd").parse("1991-03-02");
@@ -54,6 +54,58 @@ public class StudentControllerValidTest {
                 .getResponse()
                 .getContentAsString();
         String response = "{\"message\":\"newStudentEntity.studentDtoList[0].firstName: must not be empty, newStudentEntity.studentDtoList[0].firstName: firstName should be between 1 and 50 characters\",\"code\":\"BAD_REQUEST\"}";
+        assertEquals(response,contentAsString);
+    }
+
+    @Test
+    void newStudentEntityNoValidLastName() throws Exception {
+        List<StudentDto> list = new ArrayList<>();
+        StudentDto studentDto = new StudentDto();
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse("1991-03-02");
+        studentDto.setFirstName("Sanya");
+        studentDto.setLastName("");
+        studentDto.setEmail("se@mail.ru");
+        studentDto.setGender("Male");
+        studentDto.setDateOfBirth(date);
+        studentDto.setCountryOfBirth("Russia");
+        list.add(studentDto);
+
+        String contentAsString = mvc.perform(
+                        post("/student")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(list))
+                )
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        String response = "{\"message\":\"newStudentEntity.studentDtoList[0].lastName: must not be blank, newStudentEntity.studentDtoList[0].lastName: lastName should be between 1 and 50 characters\",\"code\":\"BAD_REQUEST\"}";
+        assertEquals(response,contentAsString);
+    }
+
+    @Test
+    void newStudentEntityNoValidEmil() throws Exception {
+        List<StudentDto> list = new ArrayList<>();
+        StudentDto studentDto = new StudentDto();
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse("1991-03-02");
+        studentDto.setFirstName("Sanya");
+        studentDto.setLastName("Esip");
+        studentDto.setEmail("semail.ru");
+        studentDto.setGender("Male");
+        studentDto.setDateOfBirth(date);
+        studentDto.setCountryOfBirth("Russia");
+        list.add(studentDto);
+
+        String contentAsString = mvc.perform(
+                        post("/student")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(list))
+                )
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        String response = "{\"message\":\"newStudentEntity.studentDtoList[0].email: Email should be valid\",\"code\":\"BAD_REQUEST\"}";
         assertEquals(response,contentAsString);
     }
 }
